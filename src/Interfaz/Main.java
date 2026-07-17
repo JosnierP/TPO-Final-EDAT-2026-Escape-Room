e6d03df5778aa6cf685ed136c3faba8a69961343
JosnierP <pjosnier@gmail.com>
2026-07-17 15:07:18 -0300

    mejoras a la inicializacion del sistema

--- src/Interfaz/Main.java
+++ src/Interfaz/Main.java
@@ -1,9 +1,15 @@
 package Interfaz;
 
+import Clases.Sistema;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        //Inicializador del sistema
+        Sistema miSistema = new Sistema();
+
+        Menu menu = new Menu(miSistema);
     }
 
 }
